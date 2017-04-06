c2f2bbfe28d6316e35d485bf117fade07b484cb3
ferret <ferret>
2017-04-06 14:38:31 +0100

    Add ben/ grids to neuropix/

---
+++ grids/grids.neuropix/grid_alexreverbwithnoise.m
@@ -0,0 +1,35 @@
+function grid = grid_alexreverbwithnoise
+
+% essentials
+grid.sampleRate = tdt50k;  % ~50kHz
+grid.stimGenerationFunctionName = 'stimgen_loadSoundFile';
+grid.stimDir = 'e:\benware.sounds.uncalib\alexreverbwithnoise\';
+grid.stimFilename = 'alexreverb.%1.%2.%3.wav';
+
+% stimulus grid structure
+grid.stimGridTitles = {'Reverb_level', 'Stim_number', 'Repetition'};
+
+grid.stimGrid = [];
+
+for ii = 1:10
+  grid_section = createPermutationGrid(0:2, 1:2, ii);
+  idx = randperm(size(grid_section, 1));
+  grid.stimGrid(end+1:end+length(idx),:) = grid_section(idx,:);
+end
+
+% grid.stimGrid = [...
+%     createPermutationGrid(0:2,1:2,1:10); % sparseness sounds, 4 times
+% ]; % DRCs
+
+grid.repeatsPerCondition = 1;
+grid.randomiseGrid = false;
+% grid.postStimSilence = false;
+
+% set this using absolute calibration
+%grid.stimLevelOffsetDB = [16 16]+77;
+
+% compensation filter
+%grid.initFunction = 'loadCompensationFilters';
+%grid.compensationFilterFile = ...
+%'e:\auditory-objects\calibration\calib.ben.2013.04.27\compensationFilters.mat';
+%grid.compensationFilterVarNames = {'compensationFilters.L', 'compensationFilters.R'};
---
+++ grids/grids.neuropix/grid_joshpitch.m
@@ -0,0 +1,26 @@
+function grid = grid_joshpitch()
+
+  % controlling the sound presentation
+  grid.sampleRate = tdt100k;
+  grid.stimGenerationFunctionName = 'stimgen_loadSoundFile';
+  grid.stimDir = 'e:\benware.sounds.uncalib\joshpitch\';
+  grid.stimFilename = 'KerryPitchSounds2013_%1.wav';
+  
+  % stimulus grid structure
+  grid.stimGridTitles = {'ID'};  
+  grid.stimGrid = createPermutationGrid(1:15);
+
+  global CALIBRATE
+  if CALIBRATE
+    fprintf('Calibration only!!\n');
+    pause;
+    grid.stimGrid = [1];
+  end
+  
+  % sweep parameters
+  grid.postStimSilence = 0;
+  grid.repeatsPerCondition = 1;
+  
+  % set this to match nominal level of 80dB
+  grid.legacyLevelOffsetDB = 12.5;
+  
